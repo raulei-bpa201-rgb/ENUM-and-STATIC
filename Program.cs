195d982446457a776893f439bfdb1d74125b91c3
Your Name <you@example.com>
2025-10-29 11:38:34 +0400

    tasks

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace SendMessage
+{
+    internal class Program
+    {
+        public enum Menu
+        {
+            None = 0,
+            SendMessageByEmail = 1,
+            SendMessageBySms = 2,
+        }
+        static void Main(string[] args)
+        {
+            MessageSender.Quit((int)Menu.None);
+        }
+    }
+}
---
+++ SendMessage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SendMessage
+{
+    public abstract class MessageSender
+    {
+        public void SendMessage() { }
+
+        public static void Quit(int choose)
+        {
+            switch (choose)
+            {
+                case 1:
+                    EmailSender.SendMessage();
+                    break;
+                case 2:
+                    SmsSender.SendMessage();
+                    break;
+                default:
+                    Console.WriteLine("hec ne gonderilmedi");
+                    break;
+            }
+        }
+    }
+}
